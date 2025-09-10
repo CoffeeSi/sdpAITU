235114a35558939d8ee7883076ca281a8daf9ec1
CoffeeSi <ewgenik02032006@gmail.com>
2025-09-10 15:47:00 +0500

    aaaa

---
+++ week1/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Car toyotaSupra = new Car.CarBuilder()
+                    .setName("Toyota Supra")
+                    .setEngine("2JZ")
+                    .setWheels(4)
+                    .setHandDrive(true)
+                    .build();
+        System.out.println(toyotaSupra);
+    }
+}
